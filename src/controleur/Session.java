cff9479404c9aef4725cc218ac0b5014a81be857
japer21 <17979719+japer21@users.noreply.github.com>
2017-09-21 12:08:36 +0200

    Sprint 1

---
+++ src/controleur/Session.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controleur;
+
+
+/**
+ *
+ * @author jperk
+ */
+public class Session {
+    
+    public Session(){
+         
+    }
+    public TypeEcran traiterConnexion(){
+        return TypeEcran.ECRAN_ACCUEIL;
+    }
+}
